3e6df786aec6036a11eb73a06338068436d5a980
JersonGermanEdu <u20241b429@upc.edu.pe>
2026-06-14 19:18:34 -0500

    fix: Update responsive histograma

--- scripts/histograma.js
+++ scripts/histograma.js
@@ -45,7 +45,7 @@ const histogramCharts = {
         },
         options: {
             responsive: true,
-            maintainAspectRatio: true,
+            maintainAspectRatio: false,
             plugins: {
                 legend: {
                     display: false,
